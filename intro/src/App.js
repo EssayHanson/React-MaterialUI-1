586a870b507f18031410b37141e1b0dd55b8f7c1
Steven Hanson <steven.hanson@wintechnologies.net>
2020-01-06 16:26:50 +0000

    Add exercise list

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,14 +1,39 @@
 import React, { Component, Fragment } from 'react';
 import { Header, Footer } from './Layouts';
 import Exercises from './Exercises';
+import { muscles, exercises } from './store';
 
 class App extends Component {
+  state = {
+    exercises
+  }
+
+  getExercisesByGroup() {
+    return Object.entries(this.state.exercises.reduce((exercises, exercise) => {
+      const { muscles } = exercise;
+
+      exercises[muscles] = exercises[muscles] 
+        ? [...exercises[muscles], exercise]
+        : [exercise]
+
+        return exercises;
+    }, {}));
+  }
+
   render() {
+    const exercises = this.getExercisesByGroup();
+
     return (
       <Fragment>
           <Header/>
-          <Exercises/>
-          <Footer/>
+
+          <Exercises
+            exercises={exercises}
+          />
+
+          <Footer 
+            muscles={muscles}
+          />
       </Fragment>
     )
   }
